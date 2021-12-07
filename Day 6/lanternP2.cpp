9c377b28ee702b370149cd05c24a9ecac8ec39e2
ArhumJain <arhumjain10@gmail.com>
2021-12-06 17:54:56 -0800

    Day 6

--- Day 6/lanternP2.cpp
+++ Day 6/lanternP2.cpp
@@ -5,18 +5,13 @@ int main() {
     long long timerCounts[9] = {0};
     int timer;
     for (int a = 0; a < 300; a++) {
-        fin >> timer;
-        timerCounts[timer] += 1;
-        fin.ignore(1);
+        fin >> timer; timerCounts[timer] += 1; fin.ignore(1);
     }
     long long spawns = 0; long long sum = 300;
     for (int a = 0; a < 256; a++) {
         spawns = timerCounts[0]; sum += spawns;
-        for (int timer = 0; timer < 8; timer++) {
-            timerCounts[timer] = timerCounts[timer+1];
-        }
-        timerCounts[8] = spawns;
-        timerCounts[6] += spawns;
+        for (int timer = 0; timer < 8; timer++) timerCounts[timer] = timerCounts[timer+1];
+        timerCounts[8] = spawns; timerCounts[6] += spawns;
     }
     std::cout << sum << std::endl;
 }
